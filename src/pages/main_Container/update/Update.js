c99a4a5321750f019290c7f1a2408d96036d43e2
ankanroyofficial <ankanofficial018@gmail.com>
2022-08-06 19:27:24 +0530

    payment checked

--- src/pages/main_Container/update/Update.js
+++ src/pages/main_Container/update/Update.js
@@ -1,33 +1,48 @@
 import { View, Text, Linking } from 'react-native'
 import React from 'react'
 import RazorpayCheckout from 'react-native-razorpay';
-
+import { Colors } from '../../../constant/Colors';
+import Toast from 'react-native-simple-toast';
 // import RNUpiPayment from 'react-native-upi-payment'
 export default function Update() {
 
-const payment =()=>{
-  const options = {
-    description: 'Credits towards consultation',
-    image: 'https://i.imgur.com/3g7nmJC.png',
-    currency: 'INR',
-    key: 'rzp_live_oFEqC8chIw7LbX', // Your api key
-    amount: '100',
-    name: 'Prakash Maity',
-    prefill: {
-      email: 'prakashmaity62@gmail.com',
-      contact: '9609430604',
-      name: 'Razorpay Software'
-    },
-    theme: {color: '#F37254'}
+  const payment = () => {
+    const options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: 'INR',
+      key: 'rzp_live_oFEqC8chIw7LbX', // Your api key
+      amount: '100',
+      name: 'Prakash Maity',
+      prefill: {
+        email: 'prakashmaity62@gmail.com',
+        contact: '9609430604',
+        name: 'Razorpay Software'
+      },
+      theme: { color: Colors.purple }
+    }
+    RazorpayCheckout.open(options).then((data) => {
+      // handle success
+      console.log(`Success: ${data.razorpay_payment_id}`, data);
+
+      if (data.razorpay_payment_id) {
+        Toast.show("Payment Sucessfull")
+      }
+
+
+    }).catch((error) => {
+      // handle failure
+      // console.log("Error:", error);
+
+      if (error.error) {
+        if (error.error.reason) {
+          Toast.show(error.error.reason)
+        }
+      }
+
+
+    });
   }
-  RazorpayCheckout.open(options).then((data) => {
-    // handle success
-    alert(`Success: ${data.razorpay_payment_id}`);
-  }).catch((error) => {
-    // handle failure
-    alert(`Error: ${error.code} | ${error.description}`);
-  });
-}
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", }}>
